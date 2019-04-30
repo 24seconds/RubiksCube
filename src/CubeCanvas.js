2a46279a101e67fea7c6de5dd75c5b6560f5f4b3
GeunYeong Cheon <24crazyoung@gmail.com>
2019-05-01 00:39:11 +0900

    Implement cube rotation when mouse drag the cube.. but
    I'm not gonna use this feature because it is way too much for now.

--- src/CubeCanvas.js
+++ src/CubeCanvas.js
@@ -2,13 +2,28 @@
 import CubePiece from './CubePiece';
 import { positionArray, storeClassObject } from './shared';
 
+const rotationSmoothness = 0.1;
 export default class CubeCanvas {
   constructor(tag, name, props) {
     const element = document.createElement(tag);
     element.className = name;
+    element.style.transform = 'rotate3d(1, 0, 0, -32deg) rotate3d(0, 1, 0, 45deg)';
     this.element = element;
+
     this.props = props;
     this.cubeArray = [];
+
+    this.currentX = -32; // deg
+    this.currentY = 45; // deg
+  }
+
+  setRotate3d(x, y) {
+    this.element.style.transform = `rotate3d(1, 0, 0, ${this.currentX + rotationSmoothness * y}deg) rotate3d(0, 1, 0, ${this.currentY + rotationSmoothness * x}deg)`;
+  }
+
+  updateRotate3d(x, y) {
+    this.currentY += rotationSmoothness * x;
+    this.currentX += rotationSmoothness * y;
   }
 
   render() {
--- src/CubeContainer.js
+++ src/CubeContainer.js
@@ -3,11 +3,78 @@
 import CubeCanvas from './CubeCanvas';
 
 export default class CubeContainer {
-  constructor(tag, name) {
+  constructor(tag, name, props) {
     const element = document.createElement(tag);
     element.className = name;
+    element.onmousedown = this.onMouseDown.bind(this);
+    element.onmouseup = this.onMouseUp.bind(this);
+    element.onmousemove = this.onMouseMove.bind(this);
+    element.style.transform = 'rotate3d(1, 0, 0, 0deg) rotate3d(0, 1, 0, 0deg)';
     this.element = element;
+    this.props = props;
     this.name = name;
+    this.defaultRotation = {
+      x: 0,
+      y: 0,
+      z: 0,
+    };
+    this.mouselock = false;
+
+    this.mouseX = 0;
+    this.mouseY = 0;
+  }
+
+  lockAcquire() {
+    this.mouselock = true;
+  }
+
+  lockRelaese() {
+    this.mouselock = false;
+  }
+
+  onMouseDown(event) {
+    this.lockAcquire();
+
+    const centerX = this.element.clientWidth / 2;
+    const centerY = this.element.clientHeight / 2;
+
+    const rect = event.target.getBoundingClientRect();
+
+    const x = event.pageX - rect.left;
+    const y = event.pageY - rect.top;
+
+    this.mouseX = x - centerX;
+    this.mouseY = centerY - y;
+  }
+
+  onMouseUp() {
+    this.lockRelaese();
+
+    this.child.updateRotate3d(this.vectorX, this.vectorY);
+  }
+
+  onMouseMove(event) {
+    if (!this.mouselock) return;
+
+    const centerX = this.element.clientWidth / 2;
+    const centerY = this.element.clientHeight / 2;
+
+    const rect = event.target.getBoundingClientRect();
+
+    const x = event.pageX - rect.left;
+    const y = event.pageY - rect.top;
+
+    const currentX = x - centerX;
+    const currentY = centerY - y;
+
+    this.vectorX = currentX - this.mouseX;
+    this.vectorY = currentY - this.mouseY;
+
+    this.setRotation(currentX - this.mouseX, currentY - this.mouseY);
+  }
+
+  setRotation(x, y) {
+    this.child.setRotate3d(x, y);
   }
 
   render() {
@@ -15,5 +82,7 @@ export default class CubeContainer {
 
     const cubeCanvas = new CubeCanvas('div', 'cube-canvas', this.element);
     cubeCanvas.render();
+
+    this.child = cubeCanvas;
   }
 }
