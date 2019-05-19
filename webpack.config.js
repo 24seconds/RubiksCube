680f4e83ca1fb8945d9919807170322414111120
GeunYeong Cheon <24crazyoung@gmail.com>
2019-05-19 12:42:12 +0900

    Add bable packages to handle async await snytax

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['./src/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
